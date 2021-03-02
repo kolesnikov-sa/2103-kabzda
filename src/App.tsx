5488c753ecd47a5f72796799e691c0d3cde61b06
kolesnikov-sa <kolesnikov-sa@ya.ru>
2021-03-02 11:44:48 +0300

    Mid Lesson 02

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AppTitle/>
+            <Rating />
+            <Accordion />
+            <Rating />
+        </div>
+    );
+}
+
+function AppTitle() {
+    return <>This is APP component</>
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>Пункт 1</li>
+                <li>Пункт 2</li>
+                <li>Пункт 3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
